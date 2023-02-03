f86f25196941275d6e400c07e360678f8495cd27
srijea <121870599+srijea@users.noreply.github.com>
2023-02-03 12:13:41 +0800

    1

---
+++ zf/public/js/config.js
@@ -0,0 +1,29 @@
+var pay_list = [{
+		'type': 'alipay2',
+		'name': '支付宝-支付',
+		'text': '打开支付宝扫一扫',
+		'qr': './public/images/',
+		'pass': '',
+	},
+	{
+		'type': 'wechat',
+		'name': '微信-支付',
+		'text': '打开微信扫一扫',
+		'qr': './public/images/',
+		'pass': '',
+	},
+	{
+		'type': 'qq',
+		'name': 'QQ-支付',
+		'text': '打开QQ扫一扫',
+		'qr': './public/images/qq.jpg',
+		'pass': '',
+	},
+	{
+		'type': '说明',
+		'name': '说明',
+		'text': '自愿',
+		'qr': '',
+		'pass': '',
+	},
+];
